b7e99f4f81882127fb1ae171fa2beb1ffa48c586
Gaurik-Gupta <125227706+Gaurik-Gupta@users.noreply.github.com>
2024-05-12 18:49:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    recognition.start();
+}
+
+recognition.onresult = function (event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    if (content == "Poppins") {
+        document.getElementById("text_input").style.fontFamily = "Poppins";
+    }
+    if (content == "Roboto") {
+        document.getElementById("text_input").style.fontFamily = "Roboto";
+    }
+    if (content == "Montserrat") {
+        document.getElementById("text_input").style.fontFamily = "Montserrat";
+    }
+    if (content == "Oswald") {
+        document.getElementById("text_input").style.fontFamily = "Oswald";
+    }
+
+}
+
+function setup() {
+}
+function draw() {
+    
+}
 
